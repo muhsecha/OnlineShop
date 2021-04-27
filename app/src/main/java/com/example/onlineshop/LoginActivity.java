cfa507c53809449f29968c4281df346dee9b2f99
@muhammadsecha2 <muhammadsecha2@gmail.com>
2021-04-27 14:57:28 +0700

    push layout login register apps

---
+++ app/src/main/java/com/example/onlineshop/LoginActivity.java
@@ -0,0 +1,44 @@
+package com.example.onlineshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button btn_register,btn_login;
+    TextView tv_frgt_pass;
+    EditText et_email,et_pass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        tv_frgt_pass = (TextView) findViewById(R.id.tv_frgt_pass);
+        et_email = (EditText) findViewById(R.id.et_login_email);
+        et_pass = (EditText) findViewById(R.id.et_login_pass);
+        btn_register = (Button) findViewById(R.id.btn_register);
+        btn_login = (Button) findViewById(R.id.btn_login);
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+            }
+        });
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(LoginActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/onlineshop/RegisterActivity.java
@@ -0,0 +1,43 @@
+package com.example.onlineshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    Button btn_register,btn_login;
+    EditText et_name,et_phone,et_email,et_pass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        btn_register = findViewById(R.id.btn_regis_register);
+        btn_login = findViewById(R.id.btn_log_register);
+        et_name = findViewById(R.id.et_register_name);
+        et_phone = findViewById(R.id.et_register_phone);
+        et_email = findViewById(R.id.et_register_email);
+        et_pass = findViewById(R.id.et_register_pass);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+            }
+        });
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(RegisterActivity.this, "Register success", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
