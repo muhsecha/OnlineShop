b0169776d503e41072546f77e459993558bdc97b
Hasan Wijaya <hasanwijaya.xrpl2018smkrus@gmail.com>
2021-04-28 11:11:30 +0700

    add logic register and login

--- app/src/main/java/com/example/onlineshop/UI/LoginActivity.java
+++ app/src/main/java/com/example/onlineshop/UI/LoginActivity.java
@@ -2,8 +2,11 @@ package com.example.onlineshop.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,24 +14,36 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.example.onlineshop.R;
 
 public class LoginActivity extends AppCompatActivity {
-
-    Button btn_register,btn_login;
-    TextView tv_frgt_pass;
-    EditText et_email,et_pass;
+    private Button btn_register,btn_login;
+    private TextView tv_frgt_pass;
+    private EditText et_email,et_pass;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        tv_frgt_pass = (TextView) findViewById(R.id.tv_frgt_pass);
-        et_email = (EditText) findViewById(R.id.et_login_email);
-        et_pass = (EditText) findViewById(R.id.et_login_pass);
-        btn_register = (Button) findViewById(R.id.btn_register);
-        btn_login = (Button) findViewById(R.id.btn_login);
+        tv_frgt_pass = findViewById(R.id.tv_frgt_pass);
+        et_email = findViewById(R.id.et_login_email);
+        et_pass = findViewById(R.id.et_login_pass);
+        btn_register = findViewById(R.id.btn_register);
+        btn_login = findViewById(R.id.btn_login);
+
+        progressDialog = new ProgressDialog(this);
+        SharedPreferences sp = getSharedPreferences("login", MODE_PRIVATE);
+        sp.edit().putString("logged", sp.getString("logged", "missing")).apply();
 
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,10 +54,73 @@ public class LoginActivity extends AppCompatActivity {
 //                startActivity(new Intent(getApplicationContext(),RegisterActivity.class));
             }
         });
+
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(),CreateShopActivity.class));
+                String email = et_email.getText().toString().trim();
+                String password = et_pass.getText().toString().trim();
+
+                boolean isEmpty = false;
+
+                if (email.isEmpty()) {
+                    isEmpty = true;
+                    et_email.setError("Required");
+                }
+
+                if (password.isEmpty()) {
+                    isEmpty = true;
+                    et_pass.setError("Required");
+                }
+
+                if (!isEmpty) {
+                    progressDialog.setTitle("Loading...");
+                    progressDialog.show();
+
+                    AndroidNetworking.post("https://api-olshop.ascomycota.my.id/api/login")
+                            .addBodyParameter("email", email)
+                            .addBodyParameter("password", password)
+                            .setPriority(Priority.MEDIUM)
+                            .build()
+                            .getAsJSONObject(new JSONObjectRequestListener() {
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                    try {
+                                        String status = response.getString("status");
+
+                                        if (status.equals("success")) {
+                                            SharedPreferences sp = getSharedPreferences("login", MODE_PRIVATE);
+                                            sp.edit().putString("logged", response.getString("data")).apply();
+
+                                            Toast.makeText(getApplicationContext(), "sukses", Toast.LENGTH_SHORT).show();
+
+                                            /*Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                            startActivity(intent);
+                                            finish();*/
+                                            progressDialog.dismiss();
+                                        } else {
+                                            Toast.makeText(getApplicationContext(), "gagal", Toast.LENGTH_SHORT).show();
+                                            progressDialog.dismiss();
+                                        }
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+
+                                @Override
+                                public void onError(ANError anError) {
+                                    progressDialog.dismiss();
+
+                                    if (anError.getErrorCode() != 0) {
+                                        Log.d("TAG", "onError errorCode : " + anError.getErrorCode());
+                                        Log.d("TAG", "onError errorBody : " + anError.getErrorBody());
+                                        Log.d("TAG", "onError errorDetail : " + anError.getErrorDetail());
+                                    } else {
+                                        Log.d("TAG", "onError errorDetail : " + anError.getErrorDetail());
+                                    }
+                                }
+                            });
+                }
             }
         });
     }
--- app/src/main/java/com/example/onlineshop/UI/RegisterActivity.java
+++ app/src/main/java/com/example/onlineshop/UI/RegisterActivity.java
@@ -2,20 +2,31 @@ package com.example.onlineshop.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.example.onlineshop.R;
 
 public class RegisterActivity extends AppCompatActivity {
-
-    Button btn_register,btn_login;
-    EditText et_name,et_phone,et_email,et_pass;
+    private Button btn_register,btn_login;
+    private EditText et_name,et_phone,et_email,et_pass, et_confirm_pass;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +39,9 @@ public class RegisterActivity extends AppCompatActivity {
         et_phone = findViewById(R.id.et_register_phone);
         et_email = findViewById(R.id.et_register_email);
         et_pass = findViewById(R.id.et_register_pass);
+        et_confirm_pass = findViewById(R.id.et_confirm_password);
+
+        progressDialog = new ProgressDialog(this);
 
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,7 +54,92 @@ public class RegisterActivity extends AppCompatActivity {
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(RegisterActivity.this, "Register success", Toast.LENGTH_SHORT).show();
+                String name = et_name.getText().toString().trim();
+                String email = et_email.getText().toString().trim();
+                String phoneNumber = et_phone.getText().toString().trim();
+                String password = et_pass.getText().toString().trim();
+                String confirmPassword = et_confirm_pass.getText().toString().trim();
+
+                boolean isEmpty = false;
+                boolean isInvalidPassword = false;
+
+                if (name.isEmpty()) {
+                    isEmpty = true;
+                    et_name.setError("Required");
+                }
+
+                if (email.isEmpty()) {
+                    isEmpty = true;
+                    et_email.setError("Required");
+                }
+
+                if (phoneNumber.isEmpty()) {
+                    isEmpty = true;
+                    et_phone.setError("Required");
+                }
+
+                if (password.isEmpty()) {
+                    isEmpty = true;
+                    et_pass.setError("Required");
+                }
+
+                if (confirmPassword.isEmpty()) {
+                    isEmpty = true;
+                    et_confirm_pass.setError("Required");
+                }
+
+                if (!password.equals(confirmPassword)) {
+                    isInvalidPassword = true;
+                    et_confirm_pass.setError("Password & Confirm Password don't match");
+                }
+
+
+                if (!isEmpty && !isInvalidPassword) {
+                    progressDialog.setTitle("Loading...");
+                    progressDialog.show();
+
+                    AndroidNetworking.post("https://api-olshop.ascomycota.my.id/api/register")
+                            .addBodyParameter("name", name)
+                            .addBodyParameter("email", email)
+                            .addBodyParameter("phone_number", phoneNumber)
+                            .addBodyParameter("password", password)
+                            .addBodyParameter("password_confirmation", confirmPassword)
+                            .setPriority(Priority.MEDIUM)
+                            .build()
+                            .getAsJSONObject(new JSONObjectRequestListener() {
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                    try {
+                                        String status = response.getString("status");
+
+                                        if (status.equals("success")) {
+                                            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                                            startActivity(intent);
+                                            finish();
+                                            progressDialog.dismiss();
+                                        } else {
+                                            Toast.makeText(getApplicationContext(), "gagal", Toast.LENGTH_SHORT).show();
+                                            progressDialog.dismiss();
+                                        }
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+
+                                @Override
+                                public void onError(ANError anError) {
+                                    progressDialog.dismiss();
+
+                                    if (anError.getErrorCode() != 0) {
+                                        Log.d("TAG", "onError errorCode : " + anError.getErrorCode());
+                                        Log.d("TAG", "onError errorBody : " + anError.getErrorBody());
+                                        Log.d("TAG", "onError errorDetail : " + anError.getErrorDetail());
+                                    } else {
+                                        Log.d("TAG", "onError errorDetail : " + anError.getErrorDetail());
+                                    }
+                                }
+                            });
+                }
             }
         });
     }
