55b55d1165e0162a5446ad4a410e471eaaae9c0c
@muhammadsecha2 <muhammadsecha2@gmail.com>
2021-05-03 11:25:27 +0700

    add dashboard layout

---
+++ app/src/main/java/com/example/onlineshop/UI/DashboardActivity.java
@@ -0,0 +1,50 @@
+package com.example.onlineshop.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.onlineshop.Fragment.CategoryFragment;
+import com.example.onlineshop.Fragment.ProductFragment;
+import com.example.onlineshop.R;
+import com.example.onlineshop.adapters.ViewPagerAdapter;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.tabs.TabLayout;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    MaterialToolbar materialToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.view_pager);
+        materialToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(materialToolbar);
+        getTabs();
+    }
+
+    public void getTabs(){
+        final ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        new Handler().post(new Runnable() {
+            @Override
+            public void run() {
+                viewPagerAdapter.addFragment(ProductFragment.getInstance(),"Produk");
+                viewPagerAdapter.addFragment(CategoryFragment.getInstance(),"Kategori");
+
+                viewPager.setAdapter(viewPagerAdapter);
+                tabLayout.setupWithViewPager(viewPager);
+
+                tabLayout.getTabAt(0).setIcon(R.drawable.box_white);
+                tabLayout.getTabAt(1).setIcon(R.drawable.list_white);
+            }
+        });
+    }
+}
