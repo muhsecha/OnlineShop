149ca89ef8439de31ec6a1bfecbb861b9073f857
@muhammadsecha2 <muhammadsecha2@gmail.com>
2021-05-04 13:35:32 +0700

    add feat info shop

---
+++ app/src/main/java/com/example/onlineshop/UI/ShopInfoActivity.java
@@ -0,0 +1,16 @@
+package com.example.onlineshop.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.onlineshop.R;
+
+public class ShopInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_info);
+    }
+}
