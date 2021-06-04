7eb422768562add123e3315b3859fd6b6eb41b69
@muhammadsecha2 <muhammadsecha2@gmail.com>
2021-06-04 14:18:06 +0700

    add feat: karyawan

---
+++ app/src/main/java/com/example/onlineshop/activities/EmployeeActivity.java
@@ -0,0 +1,16 @@
+package com.example.onlineshop.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.onlineshop.R;
+
+public class EmployeeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee);
+    }
+}
